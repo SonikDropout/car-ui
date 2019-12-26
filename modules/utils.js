ed2a6cedb8e9ecc56b4b413a7014bdc75bd8e4a0
Сеничкин Василий Николаевич <v.senichkin@inenergy.local>
2019-12-26 19:10:56 +0300

    update some styles and icons

--- modules/utils.js
+++ modules/utils.js
@@ -1,7 +1,21 @@
-export function createElement(tag, attributes) {
+exports.createElement = function(tag, attributes) {
   const element = document.createElement(tag);
   for (const key in attributes) {
     element[key] = attributes[key];
   }
   return element;
+}
+
+exports.collectTriggeredActions = function (event) {
+  const triggerdActions = [];
+  let currentNode = event.target;
+  while (currentNode.parentElement) {
+    if (currentNode.dataset.action)
+      triggerdActions.push({
+        type: currentNode.dataset.action,
+        target: currentNode
+      });
+    currentNode = currentNode.parentElement;
+  }
+  return triggerdActions;
 }
