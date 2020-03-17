9b6bb5dc69bc9374aedfc692aff8a94b60a67f2b
Ефимов Александр <a.efimov@inenergy.ru>
2020-03-17 13:52:05 +0300

    fix device detecting and mounting

--- src/utils/USBManager.js
+++ src/utils/USBManager.js
@@ -3,6 +3,7 @@ const driveList = require('drivelist');
 const EventEmitter = require('events');
 const { exec } = require('child_process');
 const { isPi } = require('../constants');
+const { delay } = require('./others');
 
 class USBDetector extends EventEmitter {
   constructor() {
@@ -11,6 +12,7 @@ class USBDetector extends EventEmitter {
     this._findDrive();
     this._listenAdd.bind(this)();
     this._listenRemove.bind(this)();
+    this._findDrive = delay(this._findDrive, 1000);
     this.stopMonitoring = usbDetect.stopMonitoring;
   }
 
@@ -37,25 +39,27 @@ class USBDetector extends EventEmitter {
     driveList.list((error, drives) => {
       if (error) return;
       const drive = drives.find(this._isSuitableDrive);
-      console.log('Found drives:', drives);
       if (!drive) return;
       if (drive.mountpoints[0]) this.emit('connect', drive.mountpoints[0].path);
-      else
-        exec(
-          `sudo mount ${drive.device} /media/usb1`,
-          (error, stdout, stderr) => {
-            if (error) {
-              console.error('Error mounting device:', error.message);
-              return;
-            }
-            if (stderr) {
-              console.error('Error mounting device:', stderr);
-              return;
-            }
-            this.emit('connect', drive.mountpoints[0].path);
-          }
-        );
+      else this._mountDevice(drive);
     });
+  }
+
+  _mountDevice(drive) {
+    exec(
+      `sudo mkir /media/usb1\nsudo mount ${drive.device}1 /media/usb1`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error('Error mounting device:', error.message);
+          return;
+        }
+        if (stderr) {
+          console.error('Error mounting device:', stderr);
+          return;
+        }
+        this.emit('connect', drive.mountpoints[0].path);
+      }
+    );
   }
 
   _isSuitableDrive(drive) {
---
+++ src/utils/others.js
@@ -0,0 +1,11 @@
+function delay(f, ms) {
+  let timeout;
+  return function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => f.apply(this, arguments), ms);
+  };
+}
+
+module.exports = {
+  delay
+}
