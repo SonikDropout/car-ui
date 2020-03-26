c12370b8e89cc28c7462045a2724899f4a517d4f
Ефимов Александр <a.efimov@inenergy.ru>
2020-03-26 14:30:33 +0300

    use gpio alert instad of interrupt

--- src/utils/GPIOManager.js
+++ src/utils/GPIOManager.js
@@ -6,56 +6,29 @@ const { randInt } = require('./numagic');
 class GPIOManager extends EventEmitter {
   constructor() {
     super();
-    this.rpmInput = new Gpio(INPUT_PIN, { mode: Gpio.INPUT, timeout: 1000, edge: Gpio.FALLING_EDGE });
+    this.rpmInput = new Gpio(INPUT_PIN, { mode: Gpio.INPUT, alert: true });
     this.dmOutput = new Gpio(OUTPUT_PIN, { mode: Gpio.OUTPUT });
     this.dmOutput.hardwarePwmWrite(100000, GROUND_RESISTANCE.low.dutyCycle);
     this.rpsCount = 0;
-    this.rpmInput.on('interrupt', this.handleInterrupt.bind(this));
+    this.intervalStart = Date.now();
+    this.rpmInput.on('alert', this.handleAlert.bind(this));
   }
 
   changeDriveMode(mode) {
     this.dmOutput.hardwarePwmWrite(100000, GROUND_RESISTANCE[mode].dutyCycle);
   }
 
-  countRPM() {
-    setInterval(this.readRPS);
-    setTimeout(this.emitRPM, 1000);
-  }
-
-  handleInterrupt(level) {
-    if (level === Gpio.TIMEOUT) {
+  handleAlert(level) {
+    if (level) {
+      this.rpsCount++;
+    }
+    const now = Date.now();
+    if (now - this.intervalStart > 1000) {
       this.emit('rpmMeasure', this.rpsCount * 60);
       this.rpsCount = 0;
-    } else if (!level) {
-      this.rpsCount++;
+      this.intervalStart = now;
     }
   }
 }
 
-class GPIOMock extends EventEmitter {
-  constructor() {
-    super();
-    this.rpm = 800;
-    this.emitMockValue();
-  }
-
-  changeResistancePWM(key, dutyCycle) {
-    console.log(`Changing ${key} dutyCycle to ${dutyCycle}`);
-  }
-
-  emitMockValue() {
-    setInterval(() => {
-      this.emit('rpmMeasure', this.rpm);
-      this.rpm += randInt(-5, 5);
-    });
-  }
-
-  changeDriveMode(mode) {
-    console.log('Chnaging drive mode to:', mode);
-  }
-}
-
-module.exports = {
-  GPIOManager,
-  GPIOMock,
-};
+module.exports = GPIOManager;
