f9e893a6227584c3159121230bcd6bdd53e66af0
Ефимов Александр <a.efimov@inenergy.ru>
2020-04-08 13:06:55 +0300

    fix stupid mistake

--- src/components/stores.js
+++ src/components/stores.js
@@ -25,7 +25,7 @@ const batteryData = derived(carData, ($carData) =>
   getValuesByKeys($carData, Object.keys(BATTERY_CHARACTERISTICS))
 );
 
-let timeStart;
+let timeStart = 0;
 
 const lastGraphPoints = derived(carData, ($carData) =>
   STORED_VALUES.map((valName) =>
