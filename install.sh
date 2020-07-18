b1ec561898522ee6e5878d09dd37d0edeaf3ab98
Alexander Efimov <efimokthefatass@gmail.com>
2020-07-18 18:29:36 +0300

    update installation script and instructions

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # INSTALL REQUIRED PACKAGES
 sudo apt-get update &&
-sudo apt-get -y install libbluetooth-dev pigpio &&
+sudo apt-get -y install libudev-dev libbluetooth-dev pigpio &&
 
 # MAIN APP INSTALLATION
 ELECTRON_MIRROR="https://cdn.npm.taobao.org/dist/electron/" &&
@@ -10,11 +10,9 @@ sudo mkdir /opt/bt-car &&
 sudo mv dist/linux-armv7l-unpacked/** /opt/bt-car/ &&
 
 # MAIN APP AUTOSTART
-sudo su
-echo '#!/bin/sh' > /etc/rc.local
-echo 'su -s /bin/bash -c startx pi&' >> /etc/rc.local
-echo 'exit 0' >> /etc/rc.local
-echo 'allowed_users=anybody' >> /etc/X11/Xwrapper.config
-exit
 echo 'sudo /opt/bt-car/CarController' > ~/.xinitrc
 chmod +x ~/.xinitrc
+
+while read p; do sudo systemctl disable "$p"; done < unused-packages.list
+
+reboot
