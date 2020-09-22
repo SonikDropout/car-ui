ed50dcd137b0df22ffee8912ce9e39bc3e63c712
Ефимов Александр <a.efimov@inenergy.ru>
2020-09-22 12:25:41 +0300

    udpate install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,7 @@ sudo apt-get update
 sudo apt-get -y install libudev-dev libbluetooth-dev pigpio
 
 # MAIN APP INSTALLATION
+nvm use 8
 ELECTRON_MIRROR="https://cdn.npm.taobao.org/dist/electron/"
 npm i
 npm run build
